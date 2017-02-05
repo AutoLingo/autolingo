72c866001ca1c3957bb5e928f099e78bbe6d054a
Adam M. Intrator <adam.intra2r@gmail.com>
2017-02-05 14:43:06 -0500

    LanguageMessage: 'sweetalert' typo fix

--- app/components/LanguageMessage.jsx
+++ app/components/LanguageMessage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
-import sweetAlert from 'sweetAlert';
+import sweetAlert from 'sweetalert';
 import { setUser } from '../actionCreators/user';
 import store from '../store';
 import Dropdown from 'react-simple-dropdown';
